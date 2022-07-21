24cadd4a0e789abf6af62ec9f2f83eccb8f25be1
Michael Milligan <FunkTek@gmail.com>
2022-07-21 01:08:32 -0400

    Updated typography for ExtraInformation partial. Corrected json serialization used on PrimaryAddress.

--- FairwindsCMS.Domain/Clients/CustomerService.cs
+++ FairwindsCMS.Domain/Clients/CustomerService.cs
@@ -18,48 +18,6 @@ namespace FairwindsCMS.Domain.Clients
         public async Task<List<Customer>> GetAll()
         {
             List<Customer> customerList = new();
-#if DEBUG
-            if (customerList.Count <= 0)
-            {
-                customerList.Add(new Customer
-                {
-                    FirstName = "John",
-                    LastName = "Doe",
-                    DateBirth = Convert.ToDateTime("11/01/2000"),
-                    CustomerNumber = 012345,
-                    Email = "John.Doe@mailthem.org",
-                    JoinDate = DateTime.Today,
-                    MobilePhoneNumber = "386-867-5309",
-                    Ssn = "001-00-0001",
-                    PrimaryAddress = new PrimaryAddress
-                    {
-                        AddressLine1 = "1213 Some Street",
-                        City = "Nowhere",
-                        State = "Florida",
-                        ZipCode = "98754"
-                    }
-                });
-                customerList.Add(new Customer
-                {
-                    FirstName = "Jane",
-                    LastName = "Doe",
-                    DateBirth = Convert.ToDateTime("01/01/1997"),
-                    CustomerNumber = 03456,
-                    Email = "Jane.Doe@mailthem.org",
-                    JoinDate = DateTime.Today,
-                    MobilePhoneNumber = "386-555-5555",
-                    Ssn = "001-00-0002",
-                    PrimaryAddress = new PrimaryAddress
-                    {
-                        AddressLine1 = "3121 Some Street",
-                        City = "Nowhere",
-                        State = "Florida",
-                        ZipCode = "98754"
-                    }
-                });
-            }
-            return customerList;
-#endif
             HttpResponseMessage response = await _httpClient.GetAsync(string.Empty);
 
             if (response.IsSuccessStatusCode)
--- FairwindsCMS.Domain/Models/PrimaryAddress.cs
+++ FairwindsCMS.Domain/Models/PrimaryAddress.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace FairwindsCMS.Domain.Models
@@ -7,24 +8,24 @@ namespace FairwindsCMS.Domain.Models
     {
         [Display(Name = "Address Line")]
         [MaxLength(255)]
-        [JsonPropertyName("address_line_1")]
+        [JsonProperty("address_line_1")]
         [Required]
         public string? AddressLine1 { get; set; }
 
         [Required]
         [MaxLength(255)]
-        [JsonPropertyName("city")]
+        [JsonProperty("city")]
         public string? City { get; set; }
 
         [Required]
         [MaxLength(30)]
-        [JsonPropertyName("state")]
+        [JsonProperty("state")]
         public string? State { get; set; }
 
         [Required]
         [StringLength(9, MinimumLength = 5, ErrorMessage = "Zip Code must be between 5 and 9 characters.")]
         [Display(Name = "Zip Code")]
-        [JsonPropertyName("zip_code")]
+        [JsonProperty("zip_code")]
         public string? ZipCode { get; set; }
     }
 }
